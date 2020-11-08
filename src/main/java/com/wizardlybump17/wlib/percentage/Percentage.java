5ecff390862e628e773bac26d73764e49aeefea1
WizardlyBump17 <davicraft130@gmail.com>
2020-11-07 22:15:33 -0300

    A simple class with a method to check percentages

---
+++ src/main/java/com/wizardlybump17/wlib/percentage/Percentage.java
@@ -0,0 +1,8 @@
+package com.wizardlybump17.wlib.percentage;
+
+public class Percentage {
+
+    public static boolean check(double percentage) {
+        return Math.random() * 100 <= percentage;
+    }
+}
