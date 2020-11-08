9841c218fd709507fd5ced69d59f5630777424fb
WizardlyBump17 <davicraft130@gmail.com>
2020-11-07 22:09:20 -0300

    Added protected plugin variable

--- src/main/java/com/wizardlybump17/wlib/command/WCommand.java
+++ src/main/java/com/wizardlybump17/wlib/command/WCommand.java
@@ -5,7 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class WCommand implements CommandExecutor {
 
+    protected final JavaPlugin plugin;
+
     public WCommand(JavaPlugin plugin, String commandName) {
+        this.plugin = plugin;
         plugin.getCommand(commandName).setExecutor(this);
         plugin.getLogger().info("Registered the command executor of " + commandName + " command");
     }
