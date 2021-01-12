9a85b9c2a6cfb1943f8b84e261692f7c46d37800
WizardlyBump17 <davicraft130@gmail.com>
2021-01-12 17:39:49 +0000

    Fixing repo...

--- src/src/main/java/com/wizardlybump17/wlib/percentage/Percentage.java
+++
@@ -1,8 +0,0 @@
-package com.wizardlybump17.wlib.percentage;
-
-public class Percentage {
-
-    public static boolean check(double percentage) {
-        return Math.random() * 100 <= percentage;
-    }
-}
