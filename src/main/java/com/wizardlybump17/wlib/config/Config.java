2b547111ce878f89dc5ba9a10327b9330e28526b
BuildTools <unconfigured@null.spigotmc.org>
2021-05-04 22:27:29 +0000

    added some methods

--- src/main/java/com/wizardlybump17/wlib/config/Config.java
+++ src/main/java/com/wizardlybump17/wlib/config/Config.java
@@ -1,14 +1,58 @@
 package com.wizardlybump17.wlib.config;
 
+import lombok.Data;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Data
 public class Config extends YamlConfiguration {
+
+    private final JavaPlugin plugin;
+    private final String name;
+    private final File file;
+    private boolean loaded;
+    
+    public void saveDefaultConfig() {
+        plugin.saveResource(name, false);
+    }
+
+    public void saveConfig() {
+        try {
+            save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reloadConfig() {
+        try {
+            load(file);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Object get(String path, Object def) {
+        if (!loaded) {
+            loaded = true;
+            try {
+                load(file);
+            } catch (FileAlreadyExistsException ignored) {
+            } catch (IOException | InvalidConfigurationException e) {
+                e.printStackTrace();
+            }
+        }
+        return super.get(path, def);
+    }
 
     public Map<String, Object> getMap(String path, Map<String, Object> def) {
         Object object = get(path);
@@ -30,14 +74,20 @@ public class Config extends YamlConfiguration {
         }
         return map;
     }
-    
+
+    public String getFancyString(String path, String def) {
+        String string = getString(path, def);
+        if (string == null)
+            return null;
+        return def.replace("\\n", "\n").replace('&', '§');
+    }
+
+    public String getFancyString(String path) {
+        return getFancyString(path, null);
+    }
+
     public static Config load(String filePath, JavaPlugin plugin) {
-        Config config = new Config();
-        try {
-            config.load(new File(plugin.getDataFolder(), filePath));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return config;
+        File file = new File(plugin.getDataFolder(), filePath);
+        return new Config(plugin, filePath, file);
     }
 }
