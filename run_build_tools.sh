81a8e302b86b1a6fec21483dfcc78b75c17768d7
WizardlyBump17 <davicraft130@gmail.com>
2021-08-21 16:00:05 -0300

    not it MUST work

--- run_build_tools.sh
+++
@@ -1,6 +0,0 @@
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -Xmx1G -jar BuildTools.jar --rev 1.8.8
-java -Xmx1G -jar BuildTools.jar --rev 1.12.2
-java -Xmx1G -jar BuildTools.jar --rev 1.13.2
-java -Xmx1G -jar BuildTools.jar --rev 1.15.2
-java -Xmx1G -jar BuildTools.jar --rev 1.16.5
