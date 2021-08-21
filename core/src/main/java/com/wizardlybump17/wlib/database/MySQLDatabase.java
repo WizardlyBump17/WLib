025159e0ccbc59ceccd55b79f534427c47881c59
WizardlyBump17 <davicraft130@gmail.com>
2021-08-21 15:10:34 -0300

    added the simple command replacement to MySQLDatabase

--- core/src/main/java/com/wizardlybump17/wlib/database/MySQLDatabase.java
+++ core/src/main/java/com/wizardlybump17/wlib/database/MySQLDatabase.java
@@ -1,10 +1,17 @@
 package com.wizardlybump17.wlib.database;
 
+import com.wizardlybump17.wlib.util.MapUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.PreparedStatement;
+import java.util.Map;
 import java.util.Properties;
 
 public class MySQLDatabase extends Database {
+
+    private static final Map<String, String> COMMAND_REPLACEMENTS = MapUtils.<String, String>builder()
+            .put("AUTOINCREMENT", "AUTO_INCREMENT")
+            .build();
 
     protected MySQLDatabase(Properties properties, JavaPlugin plugin) {
         super(plugin, properties);
@@ -15,6 +22,23 @@ public class MySQLDatabase extends Database {
         return "jdbc:mysql://" + properties.getProperty("host", "localhost") + ':' +
                 properties.getProperty("port", "3306") + '/' +
                 properties.getProperty("database", getPlugin().getName().toLowerCase());
+    }
+
+    @Override
+    public Database update(String command, Object... replacements) {
+        return super.update(replaceCommand(command), replacements);
+    }
+
+    @Override
+    public PreparedStatement returnUpdate(String command, Object... replacements) {
+        return super.returnUpdate(replaceCommand(command), replacements);
+    }
+
+    private static String replaceCommand(String command) {
+        command = command.toLowerCase();
+        for (Map.Entry<String, String> entry : COMMAND_REPLACEMENTS.entrySet())
+            command = command.replace(entry.getKey().toLowerCase(), entry.getValue().toLowerCase());
+        return command;
     }
 
     public static String getType() {
