1589f485da149b32503dd18254be8d516303191a
WizardlyBump17 <davicraft130@gmail.com>
2022-02-20 16:28:25 -0300

    added more options for booleans

--- commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/BooleanReader.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/BooleanReader.java
@@ -1,6 +1,30 @@
 package com.wizardlybump17.wlib.command.args.reader;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BooleanReader extends ArgsReader<Boolean> {
+
+    private static final Set<String> TRUE = new HashSet<>();
+    private static final Set<String> FALSE = new HashSet<>();
+
+    static {
+        TRUE.add("true");
+        TRUE.add("t");
+        TRUE.add("yes");
+        TRUE.add("y");
+        TRUE.add("on");
+        TRUE.add("1");
+        TRUE.add("-y");
+
+        FALSE.add("false");
+        FALSE.add("f");
+        FALSE.add("no");
+        FALSE.add("n");
+        FALSE.add("off");
+        FALSE.add("0");
+        FALSE.add("-n");
+    }
 
     @Override
     public Class<Boolean> getType() {
@@ -9,9 +33,9 @@ public class BooleanReader extends ArgsReader<Boolean> {
 
     @Override
     public Boolean read(String string) throws ArgsReaderException {
-        if (string.equalsIgnoreCase("true"))
+        if (TRUE.contains(string.toLowerCase()))
             return true;
-        if (string.equalsIgnoreCase("false"))
+        if (FALSE.contains(string.toLowerCase()))
             return false;
         throw new ArgsReaderException("expected a boolean but got " + string);
     }
