587eaa01bf1a9a0fbc36ec2237f694ee6b54218b
WizardlyBump17 <davicraft130@gmail.com>
2022-03-10 23:30:38 -0300

    make the ArrayReader return the given String if it is not a valid array pattern

--- commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArrayReader.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArrayReader.java
@@ -4,7 +4,7 @@ public abstract class ArrayReader<T> extends ArgsReader<T> {
 
     public String[] split(String string) {
         if (!string.startsWith("\"") && !string.endsWith("\""))
-            return null;
+            return new String[]{string};
 
         return string.substring(1, string.length() - 1).replace("\\\"", "\"").split(" ");
     }
