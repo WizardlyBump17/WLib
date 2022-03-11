b5cc170af0691af16638338f2fa6b94cf3c98a1f
WizardlyBump17 <davicraft130@gmail.com>
2022-03-10 23:29:56 -0300

    update version and added v1_18_R2

---
+++ versions/v1_18_R2/src/main/java/com/wizardlybump17/wlib/adapter/v1_18_R2/util/StringUtil.java
@@ -0,0 +1,30 @@
+package com.wizardlybump17.wlib.adapter.v1_18_R2.util;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Color;
+
+import java.util.regex.Matcher;
+
+public class StringUtil extends com.wizardlybump17.wlib.adapter.util.StringUtil {
+
+    public Color getColor(String string) {
+        Matcher matcher = HEX_PATTERN.matcher(string);
+        if (!matcher.matches()) {
+            try {
+                org.bukkit.ChatColor bukkitColor = org.bukkit.ChatColor.valueOf(string.toUpperCase());
+                java.awt.Color color = bukkitColor.asBungee().getColor();
+                return Color.fromRGB(color.getRed(), color.getGreen(), color.getBlue());
+            } catch (IllegalArgumentException ignored) {
+            }
+            return null;
+        }
+
+        String hex = "#" + matcher.group(1);
+        java.awt.Color javaColor = java.awt.Color.decode(hex);
+        return Color.fromRGB(javaColor.getRed(), javaColor.getGreen(), javaColor.getBlue());
+    }
+
+    public ChatColor toBungeeColor(Color color) {
+        return ChatColor.of(new java.awt.Color(color.getRed(), color.getGreen(), color.getBlue()));
+    }
+}
