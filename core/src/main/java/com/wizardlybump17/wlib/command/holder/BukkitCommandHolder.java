9ed607cfb56a8c54dfd204df9a37baf1e8785008
WizardlyBump17 <davicraft130@gmail.com>
2022-06-05 14:07:56 -0300

    remove useless Map usage

--- core/src/main/java/com/wizardlybump17/wlib/command/holder/BukkitCommandHolder.java
+++ core/src/main/java/com/wizardlybump17/wlib/command/holder/BukkitCommandHolder.java
@@ -4,14 +4,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RequiredArgsConstructor
 @Getter
 public class BukkitCommandHolder implements CommandHolder<JavaPlugin> {
 
-    private final Map<String, Command> commands = new HashMap<>();
     private final JavaPlugin handle;
 
     public static BukkitCommandHolder of(JavaPlugin plugin) {
@@ -20,8 +16,6 @@ public class BukkitCommandHolder implements CommandHolder<JavaPlugin> {
 
     @Override
     public Command getCommand(String name) {
-        BukkitCommand bukkitCommand = new BukkitCommand(handle.getCommand(name));
-        commands.put(name, bukkitCommand);
-        return bukkitCommand;
+        return new BukkitCommand(handle.getCommand(name));
     }
 }
