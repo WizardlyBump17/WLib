645767a12df42b1708ffd84284b27bcc77b31aae
WizardlyBump17 <davicraft130@gmail.com>
2022-10-11 00:52:36 -0300

    introducing Column

---
+++ database/src/main/java/com/wizardlybump17/wlib/database/orm/Column.java
@@ -0,0 +1,19 @@
+package com.wizardlybump17.wlib.database.orm;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Overrides the column name of the field
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Column {
+
+    /**
+     * @return the column name
+     */
+    String value();
+}
