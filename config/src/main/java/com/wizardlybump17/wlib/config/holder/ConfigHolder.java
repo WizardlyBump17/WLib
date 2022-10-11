71c31ab20dbc5ff8b092107323b0799ae75c8c22
WizardlyBump17 <davicraft130@gmail.com>
2022-10-11 00:51:52 -0300

    added generic for ConfigHolder#getConfig and ConfigHolder#getHandle

--- config/src/main/java/com/wizardlybump17/wlib/config/holder/ConfigHolder.java
+++ config/src/main/java/com/wizardlybump17/wlib/config/holder/ConfigHolder.java
@@ -6,7 +6,7 @@ import com.wizardlybump17.wlib.config.Configuration;
  * Basic interface for config holders
  * </code>
  */
-public interface ConfigHolder {
+public interface ConfigHolder<H, C extends Configuration<?>> {
 
     /**
      * @return the name of the holder
@@ -17,10 +17,10 @@ public interface ConfigHolder {
      * @param name the name of the config
      * @return a config with the given name
      */
-    Configuration getConfig(String name);
+    C getConfig(String name);
 
     /**
      * @return the handle of this object
      */
-    Object getHandle();
+    H getHandle();
 }
