bd0ac273b402b5799403fcb010e418d3282be86f
WizardlyBump17 <davicraft130@gmail.com>
2022-10-15 12:19:42 -0300

    added ItemButton#customData and more constructors

--- core/src/main/java/com/wizardlybump17/wlib/inventory/item/ItemButton.java
+++ core/src/main/java/com/wizardlybump17/wlib/inventory/item/ItemButton.java
@@ -3,11 +3,14 @@ package com.wizardlybump17.wlib.inventory.item;
 import com.wizardlybump17.wlib.item.ItemBuilder;
 import com.wizardlybump17.wlib.object.Pair;
 import lombok.Data;
+import lombok.NonNull;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
 @Data
@@ -25,27 +28,52 @@ public class ItemButton {
     private final Supplier<ItemStack> item;
     @Nullable
     private final ClickAction clickAction;
+    @NonNull
+    private final Map<Object, Object> customData;
 
     public ItemButton(@NotNull ItemStack item) {
-        this(item, null);
+        this(item, null, new HashMap<>());
     }
 
     public ItemButton(@NotNull ItemStack item, @Nullable ClickAction clickAction) {
-        this.item = () -> item;
-        this.clickAction = clickAction;
+        this(() -> item, clickAction, new HashMap<>());
+    }
+
+    public ItemButton(@NotNull ItemStack item, @Nullable ClickAction clickAction, @NonNull Map<Object, Object> customData) {
+        this(() -> item, clickAction, customData);
     }
 
     public ItemButton(@NotNull Supplier<ItemStack> item) {
-        this(item, null);
+        this(item, null, new HashMap<>());
     }
 
     public ItemButton(@NotNull Supplier<ItemStack> item, @Nullable ClickAction clickAction) {
+        this(item, clickAction, new HashMap<>());
+    }
+
+    public ItemButton(@NotNull Supplier<ItemStack> item, @Nullable ClickAction clickAction, @NonNull Map<Object, Object> customData) {
         this.item = item;
         this.clickAction = clickAction;
+        this.customData = customData;
     }
 
     public ItemButton(@NotNull Pair<ItemStack, ClickAction> pair) {
-        this.item = pair::getFirst;
-        this.clickAction = pair.getSecond();
+        this(pair::getFirst, pair.getSecond(), new HashMap<>());
+    }
+
+    public ItemButton(@NotNull Pair<ItemStack, ClickAction> pair, @NonNull Map<Object, Object> customData) {
+        this(pair::getFirst, pair.getSecond(), customData);
+    }
+
+    public ItemButton(@NonNull ItemBuilder itemBuilder) {
+        this(itemBuilder::build, null, itemBuilder.customData());
+    }
+
+    public ItemButton(@NonNull ItemBuilder itemBuilder, @Nullable ClickAction clickAction) {
+        this(itemBuilder::build, clickAction, itemBuilder.customData());
+    }
+
+    public ItemButton(@NonNull ItemBuilder itemBuilder, @Nullable ClickAction clickAction, @NonNull Map<Object, Object> customData) {
+        this(itemBuilder::build, clickAction, customData);
     }
 }
