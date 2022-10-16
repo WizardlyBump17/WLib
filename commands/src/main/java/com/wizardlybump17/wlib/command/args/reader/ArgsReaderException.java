23e89091cc29b39f0c783fb341e6d574b76fa81c
WizardlyBump17 <davicraft130@gmail.com>
2022-10-16 13:52:00 -0300

    added more constructors to ArgsReaderException

--- commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArgsReaderException.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArgsReaderException.java
@@ -5,4 +5,12 @@ public class ArgsReaderException extends Exception {
     public ArgsReaderException(String message) {
         super(message);
     }
+
+    public ArgsReaderException(Throwable cause) {
+        super(cause);
+    }
+
+    public ArgsReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
