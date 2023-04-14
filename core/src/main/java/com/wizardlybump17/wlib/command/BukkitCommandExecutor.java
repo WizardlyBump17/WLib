3b75acd266d9d28055a3505ab2f7321ed99b2d7f
WizardlyBump17 <davicraft130@gmail.com>
2023-04-14 15:12:17 -0300

    added annotations and using variables in the if statement

--- core/src/main/java/com/wizardlybump17/wlib/command/BukkitCommandExecutor.java
+++ core/src/main/java/com/wizardlybump17/wlib/command/BukkitCommandExecutor.java
@@ -5,10 +5,11 @@ import com.wizardlybump17.wlib.command.sender.GenericSender;
 import com.wizardlybump17.wlib.command.sender.PlayerSender;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.bukkit.command.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
 public class BukkitCommandExecutor implements CommandExecutor, com.wizardlybump17.wlib.command.holder.CommandExecutor {
@@ -17,16 +18,17 @@ public class BukkitCommandExecutor implements CommandExecutor, com.wizardlybump1
 
     @SneakyThrows
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         com.wizardlybump17.wlib.command.CommandSender<?> commandSender;
-        if (sender instanceof Player)
-            commandSender = new PlayerSender(((Player) sender));
-        else if (sender instanceof ConsoleCommandSender)
-            commandSender = new ConsoleSender(((ConsoleCommandSender) sender));
-        else if (sender instanceof BlockCommandSender)
-            commandSender = new com.wizardlybump17.wlib.command.sender.BlockCommandSender((BlockCommandSender) sender);
+        if (sender instanceof Player player)
+            commandSender = new PlayerSender(player);
+        else if (sender instanceof ConsoleCommandSender consoleSender)
+            commandSender = new ConsoleSender(consoleSender);
+        else if (sender instanceof BlockCommandSender blockSender)
+            commandSender = new com.wizardlybump17.wlib.command.sender.BlockCommandSender(blockSender);
         else
             commandSender = new GenericSender(sender);
+
         execute(commandSender, command.getName(), args);
         return false;
     }
