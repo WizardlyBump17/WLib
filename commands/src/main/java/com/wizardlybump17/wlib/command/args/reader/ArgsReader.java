16f6f4657b4539f0e3f7983aab3aebe2124551b0
WizardlyBump17 <davicraft130@gmail.com>
2023-04-24 16:11:06 -0300

    the ArgsReader#getType() can return null now

--- commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArgsReader.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/args/reader/ArgsReader.java
@@ -1,11 +1,20 @@
 package com.wizardlybump17.wlib.command.args.reader;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Used to read and convert the args from string to the specified type
  * @param <T> which type the string will be converted to
  */
 public abstract class ArgsReader<T> {
 
+    /**
+     * <p>The type that the string will be converted to.</p>
+     * <p>If it is {@code null} then you should use the {@link com.wizardlybump17.wlib.command.args.ArgsReaderType} annotation on your parameter</p>
+     * @return the type that the string will be converted to
+     */
+    @Nullable
     public abstract Class<T> getType();
+
     public abstract T read(String string) throws ArgsReaderException;
 }
