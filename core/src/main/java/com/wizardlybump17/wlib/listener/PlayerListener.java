7c8ebd416379c0210d45d25621644e37fa1f555e
WizardlyBump17 <davicraft130@gmail.com>
2023-10-07 18:19:51 -0300

    using the ControllerCache instead

--- core/src/main/java/com/wizardlybump17/wlib/listener/PlayerListener.java
+++ core/src/main/java/com/wizardlybump17/wlib/listener/PlayerListener.java
@@ -2,8 +2,9 @@ package com.wizardlybump17.wlib.listener;
 
 import com.wizardlybump17.wlib.WLib;
 import com.wizardlybump17.wlib.controller.player.reason.UnloadReasons;
-import com.wizardlybump17.wlib.database.controller.player.PlayerLoadable;
-import com.wizardlybump17.wlib.database.controller.player.PlayerLoadableRegistry;
+import com.wizardlybump17.wlib.database.cache.ControllerCache;
+import com.wizardlybump17.wlib.database.controller.Controller;
+import com.wizardlybump17.wlib.database.controller.player.PlayerController;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -16,14 +17,16 @@ public record PlayerListener(WLib plugin) implements Listener {
     @EventHandler
     public void loadPlayer(PlayerJoinEvent event) {
         UUID id = event.getPlayer().getUniqueId();
-        for (PlayerLoadable controller : PlayerLoadableRegistry.INSTANCE.getValues())
-            controller.loadPlayer(id);
+        for (Controller<?, ?, ?, ?> controller : ControllerCache.INSTANCE.getCache())
+            if (controller instanceof PlayerController<?, ?, ?> playerController)
+                playerController.loadPlayer(id);
     }
 
     @EventHandler
     public void unloadPlayer(PlayerQuitEvent event) {
         UUID id = event.getPlayer().getUniqueId();
-        for (PlayerLoadable controller : PlayerLoadableRegistry.INSTANCE.getValues())
-            controller.unloadPlayer(id, UnloadReasons.QUIT);
+        for (Controller<?, ?, ?, ?> controller : ControllerCache.INSTANCE.getCache())
+            if (controller instanceof PlayerController<?, ?, ?> playerController)
+                playerController.unloadPlayer(id, UnloadReasons.QUIT);
     }
 }
