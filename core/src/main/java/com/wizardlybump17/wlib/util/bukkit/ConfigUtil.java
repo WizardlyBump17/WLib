ad20adfd1417b71b11b09cce6a2cc0cf110cf5b7
WizardlyBump17 <davicraft130@gmail.com>
2023-10-11 17:05:39 -0300

    added the ConfigUtil.get(String, Map, Object) method

--- core/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
+++ core/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
@@ -2,6 +2,7 @@ package com.wizardlybump17.wlib.util.bukkit;
 
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
@@ -13,5 +14,12 @@ public class ConfigUtil {
     @SuppressWarnings("unchecked")
     public static <T> @NonNull T get(@NonNull String key, @NonNull Map<@NonNull String, @Nullable Object> map) {
         return (T) Objects.requireNonNull(map.get(key), "The key '" + key + "' is not present in the config!");
+    }
+
+    @Contract("_, _, !null -> !null")
+    @SuppressWarnings("unchecked")
+    public static <T> @Nullable T get(@NonNull String key, @NonNull Map<@NonNull String, @Nullable Object> map, @Nullable T defaultValue) {
+        Object object = map.get(key);
+        return object == null ? defaultValue : (T) object;
     }
 }
