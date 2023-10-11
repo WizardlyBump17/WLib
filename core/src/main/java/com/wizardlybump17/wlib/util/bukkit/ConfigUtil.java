694a031cf3cb4fd1d51195d8354fd3167ada92f1
WizardlyBump17 <davicraft130@gmail.com>
2023-10-11 17:08:45 -0300

    documented the ConfigUtil methods

--- core/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
+++ core/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
@@ -11,11 +11,32 @@ import java.util.Objects;
 @UtilityClass
 public class ConfigUtil {
 
+    /**
+     * <p>
+     *     Gets a value from the given {@link Map} by the given key.
+     *     If the value is null, it will throw a {@link NullPointerException}.
+     * </p>
+     * @param key the key to get the value
+     * @param map the map to get the value from
+     * @return the value
+     * @param <T> the type of the value
+     */
     @SuppressWarnings("unchecked")
     public static <T> @NonNull T get(@NonNull String key, @NonNull Map<@NonNull String, @Nullable Object> map) {
         return (T) Objects.requireNonNull(map.get(key), "The key '" + key + "' is not present in the config!");
     }
 
+    /**
+     * <p>
+     *     Gets a value from the given {@link Map} by the given key.
+     *     If the value is null, it will return the given default value.
+     * </p>
+     * @param key the key to get the value
+     * @param map the map to get the value from
+     * @param defaultValue the default value to return if the value is null
+     * @return the value or the default value
+     * @param <T> the type of the value
+     */
     @Contract("_, _, !null -> !null")
     @SuppressWarnings("unchecked")
     public static <T> @Nullable T get(@NonNull String key, @NonNull Map<@NonNull String, @Nullable Object> map, @Nullable T defaultValue) {
