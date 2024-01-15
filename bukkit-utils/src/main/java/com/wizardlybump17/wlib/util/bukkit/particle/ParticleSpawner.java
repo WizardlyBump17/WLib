7b3129fdd284fba9de3efc66455dac8b37fd5d5e
WizardlyBump17 <davicraft130@gmail.com>
2024-01-15 17:52:27 -0300

    added the ParticleSpawner class

---
+++ bukkit-utils/src/main/java/com/wizardlybump17/wlib/util/bukkit/particle/ParticleSpawner.java
@@ -0,0 +1,83 @@
+package com.wizardlybump17.wlib.util.bukkit.particle;
+
+import com.wizardlybump17.wlib.util.builder.MapBuilder;
+import com.wizardlybump17.wlib.util.bukkit.ConfigUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@SerializableAs("WLib:Particle")
+public class ParticleSpawner implements ConfigurationSerializable {
+
+    private @NonNull Particle type;
+    private int count;
+    private double offsetX;
+    private double offsetY;
+    private double offsetZ;
+    private double xAdd;
+    private double yAdd;
+    private double zAdd;
+    private double extra;
+    private @Nullable Object data;
+
+    @Override
+    public @NonNull Map<String, Object> serialize() {
+        return new MapBuilder<String, Object>(new LinkedHashMap<>())
+                .put("type", type.name())
+                .putIf("count", count, count != 1)
+                .putIfNotEmpty(
+                        "offset", new MapBuilder<String, Double>(new LinkedHashMap<>())
+                                .putIf("x", offsetX, offsetX != 0)
+                                .putIf("y", offsetY, offsetY != 0)
+                                .putIf("z", offsetZ, offsetZ != 0)
+                )
+                .putIfNotEmpty(
+                        "add", new MapBuilder<String, Double>(new LinkedHashMap<>())
+                                .putIf("x", xAdd, xAdd != 0)
+                                .putIf("y", yAdd, yAdd != 0)
+                                .putIf("z", zAdd, zAdd != 0)
+                )
+                .putIf("extra", extra, extra != 0)
+                .putIfNotNull("data", data)
+                .build();
+    }
+
+    public void spawn(@NonNull Location location) {
+        World world = Objects.requireNonNull(location.getWorld(), "The world cannot be null");
+
+        if (xAdd != 0 || yAdd != 0 || zAdd != 0)
+            location = location.clone().add(xAdd, yAdd, zAdd);
+
+        world.spawnParticle(type, location, count, offsetX, offsetY, offsetZ, extra, data);
+    }
+
+    public static @NonNull ParticleSpawner deserialize(@NonNull Map<@NonNull String, @Nullable Object> map) {
+        Map<String, Object> offset = ConfigUtil.get("offset", map, Collections.emptyMap());
+        Map<String, Object> add = ConfigUtil.get("add", map, Collections.emptyMap());
+        return new ParticleSpawner(
+                Particle.valueOf(ConfigUtil.<String>get("type", map).toUpperCase()),
+                ConfigUtil.get("count", map, 1),
+                ConfigUtil.<Number>get("x", offset, 0.0).doubleValue(),
+                ConfigUtil.<Number>get("y", offset, 0.0).doubleValue(),
+                ConfigUtil.<Number>get("z", offset, 0.0).doubleValue(),
+                ConfigUtil.<Number>get("x", add, 0.0).doubleValue(),
+                ConfigUtil.<Number>get("y", add, 0.0).doubleValue(),
+                ConfigUtil.<Number>get("z", add, 0.0).doubleValue(),
+                ConfigUtil.get("extra", map, 0.0),
+                ConfigUtil.get("data", map, (Object) null)
+        );
+    }
+}
