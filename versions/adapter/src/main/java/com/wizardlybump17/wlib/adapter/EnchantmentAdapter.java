ed15a081ec9e7947ea33dbc9cf2718e412d04749
WizardlyBump17 <davicraft130@gmail.com>
2024-04-21 21:45:33 -0300

    added the EnchantmentAdapter#registerEnchantment(Enchantment) method

--- versions/adapter/src/main/java/com/wizardlybump17/wlib/adapter/EnchantmentAdapter.java
+++ versions/adapter/src/main/java/com/wizardlybump17/wlib/adapter/EnchantmentAdapter.java
@@ -1,12 +1,17 @@
 package com.wizardlybump17.wlib.adapter;
 
+import com.wizardlybump17.wlib.util.ReflectionUtil;
 import lombok.Getter;
 import lombok.NonNull;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Field;
+
 public class EnchantmentAdapter {
+
+    public static final @NonNull Field ACCEPTING_NEW = ReflectionUtil.getField("acceptingNew", Enchantment.class);
 
     @Getter
     private static EnchantmentAdapter instance;
@@ -19,5 +24,14 @@ public class EnchantmentAdapter {
 
     public @Nullable Enchantment getEnchantment(@NonNull String string) {
         return Enchantment.getByKey(NamespacedKey.fromString(string));
+    }
+
+    public boolean registerEnchantment(@NonNull Enchantment enchantment) {
+        if (Enchantment.getByKey(enchantment.getKey()) != null || Enchantment.getByName(enchantment.getName()) != null)
+            return false;
+
+        ReflectionUtil.setFieldValue(ACCEPTING_NEW, null, true);
+        Enchantment.registerEnchantment(enchantment);
+        return true;
     }
 }
