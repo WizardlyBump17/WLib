173fae4d562f9da4d2a2fecfba33b565b73b7e8f
WizardlyBump17 <davicraft130@gmail.com>
2024-06-21 13:43:44 -0300

    added the ConfigUtil.mapOrDefault(String, Map, Supplier, Function) method

--- bukkit-utils/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
+++ bukkit-utils/src/main/java/com/wizardlybump17/wlib/util/bukkit/ConfigUtil.java
@@ -96,4 +96,25 @@ public class ConfigUtil {
             object = supplier.get();
         return object == null ? null: mapper.apply((T) object);
     }
+
+    /**
+     * <p>
+     *     Gets a value from the given {@link Map} by the given key and maps it using the given {@link Function} mapper.
+     *     If the value is {@code null}, it will use the {@link Supplier#get()} from the given default value {@link Supplier}.
+     * </p>
+     * @param key the key to get the value
+     * @param map the {@link Map} to get the value from
+     * @param defaultValue the {@link Supplier} to {@code return} the default value if the value in the {@link Map} is {@code null}
+     * @param mapper the {@link Function} to {@link Map} the value
+     * @return the value or the default value
+     * @param <T> the type of the value
+     * @param <R> the type of the mapped value
+     */
+    @SuppressWarnings("unchecked")
+    public static <T, R> @Nullable R mapOrDefault(@NonNull String key, @NonNull Map<String, Object> map, @NonNull Supplier<R> defaultValue, @NonNull Function<T, R> mapper) {
+        Object object = map.get(key);
+        if (object == null)
+            return defaultValue.get();
+        return mapper.apply((T) object);
+    }
 }
