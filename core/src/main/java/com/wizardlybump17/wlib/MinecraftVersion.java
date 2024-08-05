c14d724002dd81e6707100ea0589165a680616db
WizardlyBump17 <davicraft130@gmail.com>
2024-08-05 14:53:22 -0300

    added the unknown version
    Signed-off-by: WizardlyBump17 <davicraft130@gmail.com>

--- core/src/main/java/com/wizardlybump17/wlib/MinecraftVersion.java
+++ core/src/main/java/com/wizardlybump17/wlib/MinecraftVersion.java
@@ -5,12 +5,13 @@ import org.bukkit.Bukkit;
 
 public enum MinecraftVersion {
 
-    V1_20_6;
+    V1_20_6,
+    UNKNOWN;
 
     public static @NonNull MinecraftVersion getVersion() {
         return switch (Bukkit.getMinecraftVersion()) {
             case "1.20.6" -> V1_20_6;
-            default -> throw new IllegalStateException("Unsupported server version");
+            default -> UNKNOWN;
         };
     }
 }
