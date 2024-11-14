c68bdf09bf7737346f8b2a072c709256d5fb5094
WizardlyBump17 <davicraft130@gmail.com>
2024-11-13 22:05:59 -0300

    added the CommandMapAdapter for the other versions

---
+++ versions/v1_19_R2/src/main/java/com/wizardlybump17/wlib/adapter/v1_19_R2/command/CommandMapAdapter.java
@@ -0,0 +1,33 @@
+package com.wizardlybump17.wlib.adapter.v1_19_R2.command;
+
+import com.wizardlybump17.wlib.util.ReflectionUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.SimpleCommandMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public class CommandMapAdapter extends com.wizardlybump17.wlib.adapter.command.CommandMapAdapter {
+
+    public static final @NotNull CommandMap COMMAND_MAP = ReflectionUtil.getFieldValue(ReflectionUtil.getField("commandMap", Bukkit.getServer().getClass()), Bukkit.getServer());
+    public static final @NotNull Map<String, Command> COMMANDS = ReflectionUtil.getFieldValue(ReflectionUtil.getField("knownCommands", SimpleCommandMap.class), COMMAND_MAP);
+
+    @Override
+    public @NotNull CommandMap getCommandMap() {
+        return COMMAND_MAP;
+    }
+
+    @Override
+    public void unregisterCommand(@NotNull String command) {
+        Command removed = COMMANDS.remove(command);
+        if (removed != null)
+            removed.unregister(COMMAND_MAP);
+    }
+
+    @Override
+    public @NotNull Map<String, Command> getCommands() {
+        return COMMANDS;
+    }
+}
