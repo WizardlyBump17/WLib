faadaeab23290e8bc81979292504ec2c30725384
WizardlyBump17 <davicraft130@gmail.com>
2024-11-15 21:45:52 -0300

    made the CommandManager store commands using a Map where the key is the command name. Removed lombok

--- commands/src/main/java/com/wizardlybump17/wlib/command/CommandManager.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/CommandManager.java
@@ -7,17 +7,14 @@ import com.wizardlybump17.wlib.command.extractor.DirectCommandExtractor;
 import com.wizardlybump17.wlib.command.extractor.MethodCommandExtractor;
 import com.wizardlybump17.wlib.command.holder.CommandHolder;
 import com.wizardlybump17.wlib.command.registered.RegisteredCommand;
-import lombok.Getter;
-import lombok.NonNull;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
-@Getter
 public class CommandManager {
 
-    private final List<RegisteredCommand> commands = new ArrayList<>();
-    protected final CommandHolder<?> holder;
+    private final @NotNull Map<String, Set<RegisteredCommand>> commands = new HashMap<>();
+    protected final @NotNull CommandHolder<?> holder;
     private final @NotNull Set<CommandExtractor> commandExtractors = new HashSet<>();
 
     public CommandManager(@NotNull CommandHolder<?> holder) {
@@ -26,33 +23,41 @@ public class CommandManager {
         commandExtractors.add(new DirectCommandExtractor());
     }
 
-    public void registerCommands(Object... objects) {
-        for (Object object : objects) {
-            for (CommandExtractor extractor : commandExtractors) {
-                List<RegisteredCommand> commands = extractor.extract(this, holder, object);
-                commands.forEach(command -> command.onRegister(this));
-                this.commands.addAll(commands);
-            }
-        }
-        commands.sort(null);
+    public void registerCommands(@NotNull Object @NotNull ... objects) {
+        for (Object object : objects)
+            for (CommandExtractor extractor : commandExtractors)
+                registerCommands(extractor.extract(this, holder, object));
     }
 
     public void registerCommands(@NotNull Collection<RegisteredCommand> commands) {
-        commands.forEach(command -> command.onRegister(this));
-        this.commands.addAll(commands);
-        this.commands.sort(null);
+        commands.forEach(this::registerCommand);
+    }
+
+    public void registerCommand(@NotNull RegisteredCommand command) {
+        command.onRegister(this);
+        commands.computeIfAbsent(command.getName().toLowerCase(), $ -> new TreeSet<>()).add(command);
     }
 
     public void unregisterCommands() {
-        commands.forEach(command -> command.onUnregister(this));
+        commands.forEach((name, commands) -> {
+            commands.forEach(command -> command.onUnregister(this));
+            commands.clear();
+        });
         commands.clear();
     }
 
-    public void execute(CommandSender<?> sender, String string) throws CommandException {
+    public void execute(@NotNull CommandSender<?> sender, @NotNull String string) throws CommandException {
         if (commands.isEmpty())
             return;
 
-        for (RegisteredCommand registeredCommand : commands) {
+        int spaceIndex = string.indexOf(' ');
+        String name;
+        if (spaceIndex == -1)
+            name = string;
+        else
+            name = string.substring(0, spaceIndex);
+
+        for (RegisteredCommand registeredCommand : commands.getOrDefault(name, Set.of())) {
             CommandData command = registeredCommand.getCommand();
             CommandResult result = registeredCommand.execute(sender, string);
 
@@ -78,24 +83,33 @@ public class CommandManager {
         }
     }
 
-    @NonNull
-    public List<@NonNull String> autoComplete(@NonNull CommandSender<?> sender, @NonNull String string) {
+    public @NotNull List<@NotNull String> autoComplete(@NotNull CommandSender<?> sender, @NotNull String string) {
         return List.of();
     }
 
-    public List<RegisteredCommand> getCommand(String name) {
-        List<RegisteredCommand> commands = new ArrayList<>(this.commands.size());
-        for (RegisteredCommand command : this.commands)
-            if (command.getName().equalsIgnoreCase(name))
-                commands.add(command);
-        return commands;
+    public @NotNull List<RegisteredCommand> getCommands(@NotNull String name) {
+        Set<RegisteredCommand> commands = this.commands.get(name);
+        return commands == null ? List.of() : List.copyOf(commands);
     }
 
-    public List<RegisteredCommand> getCommands(@NotNull Object object) {
-        List<RegisteredCommand> commands = new ArrayList<>(this.commands.size());
-        for (RegisteredCommand command : this.commands)
-            if (command.isOwnedBy(object))
-                commands.add(command);
-        return commands;
+    public @NotNull List<RegisteredCommand> getCommands(@NotNull Object object) {
+        List<RegisteredCommand> result = new ArrayList<>(commands.size());
+        for (Set<RegisteredCommand> commands : this.commands.values())
+            for (RegisteredCommand command : commands)
+                if (command.isOwnedBy(object))
+                    result.add(command);
+        return result;
+    }
+
+    public @NotNull CommandHolder<?> getHolder() {
+        return holder;
+    }
+
+    public @NotNull Map<String, Set<RegisteredCommand>> getCommands() {
+        return Map.copyOf(commands);
+    }
+
+    public @NotNull Set<CommandExtractor> getCommandExtractors() {
+        return Set.copyOf(commandExtractors);
     }
 }
