7403361306854214ef267f6189bdb31cd890f398
WizardlyBump17 <davicraft130@gmail.com>
2025-10-30 20:46:11 -0300

    starting actual CommandManager tests

--- commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/CommandManagerTests.java
+++ commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/CommandManagerTests.java
@@ -1,78 +1,15 @@
 package com.wizardlybump17.wlib.command.test.rework;
 
-import com.wizardlybump17.wlib.command.rework.Command;
-import com.wizardlybump17.wlib.command.rework.executor.CommandNodeExecutor;
-import com.wizardlybump17.wlib.command.rework.input.AllowedNumberInputs;
-import com.wizardlybump17.wlib.command.rework.manager.CommandManager;
-import com.wizardlybump17.wlib.command.rework.node.IntegerCommandNode;
-import com.wizardlybump17.wlib.command.rework.node.LiteralCommandNode;
-import com.wizardlybump17.wlib.command.rework.result.CommandResult;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.wizardlybump17.wlib.command.sender.BasicCommandSender;
+import com.wizardlybump17.wlib.command.sender.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 class CommandManagerTests {
 
-    @Test
-    void test() {
-        CommandNodeExecutor<Object> executor = context -> CommandResult.successful(context, "a1");
-
-        Command command0 = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        ))
-                ))
-        );
-        Command command1 = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world1", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        ))
-                ))
-        );
-        Command command2 = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world", List.of(
-                                new IntegerCommandNode("a2", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        ))
-                ))
-        );
-        Command command3 = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor),
-                                new IntegerCommandNode("a2", List.of(
-                                        new LiteralCommandNode("hi")
-                                ), new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        )),
-                        new LiteralCommandNode("world1", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        ))
-                ))
-        );
-
-        CommandManager manager = new CommandManager();
-        manager.registerCommand("test", command0);
-        manager.registerCommand("test", command1);
-        manager.registerCommand("test", command2);
-        manager.registerCommand("test", command3);
-
-        Command expected = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor),
-                                new IntegerCommandNode("a2", List.of(
-                                        new LiteralCommandNode("hi")
-                                ), new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        )),
-                        new LiteralCommandNode("world1", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), executor)
-                        ))
-                ))
-        );
-
-        Assertions.assertEquals(expected, manager.getCommand("hello").get());
-    }
+    static final @NotNull Consumer<String> SENDER_MESSAGE_CONSUMER = System.out::println;
+    static final @NotNull CommandSender<Object> CHAD_SENDER = new BasicCommandSender<>(new Object(), "Chad", UUID.nameUUIDFromBytes("Chad".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> true);
+    static final @NotNull CommandSender<Object> BETA_SENDER = new BasicCommandSender<>(new Object(), "Beta", UUID.nameUUIDFromBytes("Beta".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> false);
 }
