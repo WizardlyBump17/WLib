e97bb432dc733db8270028b7aa8c108dfc97bd7a
WizardlyBump17 <davicraft130@gmail.com>
2025-10-30 20:16:34 -0300

    renamed and added another sender

--- commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/CommandTests.java
+++ commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/CommandTests.java
@@ -20,7 +20,8 @@ import java.util.function.Consumer;
 public class CommandTests {
 
     static final @NotNull Consumer<String> SENDER_MESSAGE_CONSUMER = System.out::println;
-    static final @NotNull CommandSender<Object> ALL_KNOWING_SENDER = new BasicCommandSender<>(new Object(), "Test", UUID.nameUUIDFromBytes("Test".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> true);
+    static final @NotNull CommandSender<Object> CHAD_SENDER = new BasicCommandSender<>(new Object(), "Chad", UUID.nameUUIDFromBytes("Chad".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> true);
+    static final @NotNull CommandSender<Object> BETA_SENDER = new BasicCommandSender<>(new Object(), "Beta", UUID.nameUUIDFromBytes("Beta".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> false);
 
     @Test
     void testSuccessHello() {
@@ -28,7 +29,7 @@ public class CommandTests {
         Command command = new Command(helloNode);
 
         CommandResult<String> expected = CommandResult.successful(0, helloNode, "hello");
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -39,7 +40,7 @@ public class CommandTests {
         Command command = new Command(new LiteralCommandNode("hello", List.of(worldNode)));
 
         CommandResult<String> expected = CommandResult.successful(1, worldNode, "hello world");
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "world"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "world"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -50,7 +51,7 @@ public class CommandTests {
         Command command = new Command(new LiteralCommandNode("hello", List.of(new LiteralCommandNode("world", List.of(hiNode)))));
 
         CommandResult<String> expected = CommandResult.successful(2, hiNode, "hello world hi");
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "world", "hi"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "world", "hi"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -68,7 +69,7 @@ public class CommandTests {
         ));
 
         CommandResult<String> expected = CommandResult.successful(1, hiNode, "hello hi");
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "hi"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "hi"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -86,7 +87,7 @@ public class CommandTests {
         ));
 
         CommandResult<String> expected = CommandResult.successful(2, hiWorldNode, "hello hi world");
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "hi", "world"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "hi", "world"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -97,7 +98,7 @@ public class CommandTests {
         Command command = new Command(helloNode);
 
         ExtraArgumentsResult<?> expected = CommandResult.extraArguments(1, helloNode);
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "world"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "world"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -113,7 +114,7 @@ public class CommandTests {
         );
 
         ExtraArgumentsResult<?> expected = CommandResult.extraArguments(2, worldNode);
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "world", "hi"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "world", "hi"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -130,7 +131,7 @@ public class CommandTests {
         ));
 
         ExtraArgumentsResult<?> expected = CommandResult.extraArguments(3, hiWorldNode);
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello", "hi", "world", "extra"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello", "hi", "world", "extra"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -144,7 +145,7 @@ public class CommandTests {
         Command command = new Command(helloNode);
 
         ExceptionResult<?> expected = CommandResult.exceptionally(0, helloNode, helloException);
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of("hello"));
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of("hello"));
 
         Assertions.assertEquals(expected, actual);
     }
@@ -154,7 +155,7 @@ public class CommandTests {
         Command command = new Command(new LiteralCommandNode("hello", context -> CommandResult.successful(context, "hello")));
 
         InsufficientArgumentsResult<?> expected = CommandResult.insufficientArguments(command);
-        CommandResult<?> actual = command.execute(ALL_KNOWING_SENDER, List.of());
+        CommandResult<?> actual = command.execute(CHAD_SENDER, List.of());
 
         Assertions.assertEquals(expected, actual);
     }
