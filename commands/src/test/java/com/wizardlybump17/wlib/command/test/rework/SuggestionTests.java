0cd4bd582ef4d6e5c12cbc30dbbe7c0cbde1dea8
WizardlyBump17 <davicraft130@gmail.com>
2025-10-30 23:47:42 -0300

    added actual tests for the suggestion system

--- commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/SuggestionTests.java
+++ commands/src/test/java/com/wizardlybump17/wlib/command/test/rework/SuggestionTests.java
@@ -5,32 +5,155 @@ import com.wizardlybump17.wlib.command.rework.input.AllowedNumberInputs;
 import com.wizardlybump17.wlib.command.rework.node.IntegerCommandNode;
 import com.wizardlybump17.wlib.command.rework.node.LiteralCommandNode;
 import com.wizardlybump17.wlib.command.rework.result.CommandResult;
+import com.wizardlybump17.wlib.command.sender.BasicCommandSender;
+import com.wizardlybump17.wlib.command.sender.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 class SuggestionTests {
 
+    static final @NotNull Consumer<String> SENDER_MESSAGE_CONSUMER = System.out::println;
+    static final @NotNull CommandSender<Object> CHAD_SENDER = new BasicCommandSender<>(new Object(), "Chad", UUID.nameUUIDFromBytes("Chad".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> true);
+    static final @NotNull CommandSender<Object> BETA_SENDER = new BasicCommandSender<>(new Object(), "Beta", UUID.nameUUIDFromBytes("Beta".getBytes()), SENDER_MESSAGE_CONSUMER, $ -> false);
+
     @Test
-    void test() {
+    @DisplayName("Test Success: root 0")
+    void testSuccess0() {
         Command command = new Command(
-                new LiteralCommandNode("hello", List.of(
-                        new LiteralCommandNode("world", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), context -> CommandResult.successful(context, "a1"))
-                        )),
-                        new LiteralCommandNode("world1", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), context -> CommandResult.successful(context, "a1"))
-                        )),
-                        new LiteralCommandNode("world2", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), context -> CommandResult.successful(context, "a1"))
-                        )),
-                        new LiteralCommandNode("world3", List.of(
-                                new IntegerCommandNode("a1", new AllowedNumberInputs.AllowedIntegerInputs.Range(10, 100), context -> CommandResult.successful(context, "a1"))
-                        ))
-                ))
+                new LiteralCommandNode(
+                        "hello",
+                        List.of(
+                                new LiteralCommandNode(
+                                        "world",
+                                        context -> CommandResult.successful(context, "hello world")
+                                )
+                        )
+                )
         );
 
-        List<String> suggestions = command.getSuggestions(null, List.of("hello", "world"));
-        System.out.println(suggestions);
+        List<String> expected = List.of("hello");
+        List<String> actual = command.getSuggestions(CHAD_SENDER, List.of());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test Success: root 1")
+    void testSuccess1() {
+        Command command = new Command(
+                new LiteralCommandNode(
+                        "hello",
+                        List.of(
+                                new LiteralCommandNode(
+                                        "world",
+                                        context -> CommandResult.successful(context, "hello world")
+                                )
+                        )
+                )
+        );
+
+        List<String> expected = List.of("hello");
+        List<String> actual = command.getSuggestions(CHAD_SENDER, List.of("he"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test Success: children 0")
+    void testSuccess2() {
+        Command command = new Command(
+                new LiteralCommandNode(
+                        "hello",
+                        List.of(
+                                new LiteralCommandNode(
+                                        "world",
+                                        context -> CommandResult.successful(context, "hello world")
+                                ),
+                                new LiteralCommandNode(
+                                        "hi",
+                                        context -> CommandResult.successful(context, "hello hi")
+                                ),
+                                new LiteralCommandNode(
+                                        "there",
+                                        context -> CommandResult.successful(context, "hello there")
+                                )
+                        )
+                )
+        );
+
+        List<String> expected = List.of("world", "hi", "there");
+        List<String> actual = command.getSuggestions(CHAD_SENDER, List.of("hello", ""));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test Success: children 1")
+    void testSuccess3() {
+        Command command = new Command(
+                new LiteralCommandNode(
+                        "hello",
+                        List.of(
+                                new LiteralCommandNode(
+                                        "world",
+                                        context -> CommandResult.successful(context, "hello world")
+                                ),
+                                new LiteralCommandNode(
+                                        "hi",
+                                        context -> CommandResult.successful(context, "hello hi")
+                                ),
+                                new IntegerCommandNode(
+                                        "there",
+                                        new AllowedNumberInputs.AllowedIntegerInputs.Range(1, 10),
+                                        context -> CommandResult.successful(context, "hello there")
+                                )
+                        )
+                )
+        );
+
+        List<String> expected = List.of("world", "hi", "1", "3", "5", "7", "10");
+        List<String> actual = command.getSuggestions(CHAD_SENDER, List.of("hello", ""));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test Success: children 2")
+    void testSuccess4() {
+        Command command = new Command(
+                new LiteralCommandNode(
+                        "hello",
+                        List.of(
+                                new LiteralCommandNode(
+                                        "world",
+                                        context -> CommandResult.successful(context, "hello world")
+                                ),
+                                new LiteralCommandNode(
+                                        "hi",
+                                        context -> CommandResult.successful(context, "hello hi")
+                                ),
+                                new IntegerCommandNode(
+                                        "there",
+                                        new AllowedNumberInputs.AllowedIntegerInputs.Range(1, 10),
+                                        context -> CommandResult.successful(context, "hello there")
+                                ),
+                                new LiteralCommandNode(
+                                        "happy",
+                                        context -> CommandResult.successful(context, "hello happy")
+                                )
+                        )
+                )
+        );
+
+        List<String> expected = List.of("world", "hi", "happy");
+        List<String> actual = command.getSuggestions(CHAD_SENDER, List.of("hello", "h"));
+
+        Assertions.assertEquals(expected, actual);
     }
 }
