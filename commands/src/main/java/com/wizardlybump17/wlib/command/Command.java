dee9ef6a27d10f6d51df00a6e29f23a86187c43c
WizardlyBump17 <davicraft130@gmail.com>
2025-11-07 17:45:29 -0300

    removed the unused methods

--- commands/src/main/java/com/wizardlybump17/wlib/command/Command.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/Command.java
@@ -4,20 +4,13 @@ import com.wizardlybump17.wlib.command.context.CommandContext;
 import com.wizardlybump17.wlib.command.exception.InputParsingException;
 import com.wizardlybump17.wlib.command.exception.InvalidInputException;
 import com.wizardlybump17.wlib.command.executor.CommandNodeExecutor;
-import com.wizardlybump17.wlib.command.input.AllowedNumberInputs;
 import com.wizardlybump17.wlib.command.node.CommandNode;
-import com.wizardlybump17.wlib.command.node.IntegerCommandNode;
 import com.wizardlybump17.wlib.command.node.LiteralCommandNode;
 import com.wizardlybump17.wlib.command.result.CommandResult;
 import com.wizardlybump17.wlib.command.sender.CommandSender;
 import com.wizardlybump17.wlib.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -181,23 +174,6 @@ public class Command {
         return new Command((LiteralCommandNode) root.merge(other.getRoot()));
     }
 
-    public static @Nullable Command createCommand(@NotNull String execution) {
-        String[] parts = execution.split(" ");
-
-        LiteralCommandNode lastNode = null;
-        for (int i = parts.length - 1; i >= 0; i--) {
-            String part = parts[i];
-            if (lastNode == null) {
-                lastNode = new LiteralCommandNode(part);
-                continue;
-            }
-
-            lastNode = new LiteralCommandNode(part, List.of(lastNode));
-        }
-
-        return new Command(lastNode);
-    }
-
     @Override
     public String toString() {
         return "Command{" +
@@ -216,83 +192,5 @@ public class Command {
     @Override
     public int hashCode() {
         return Objects.hashCode(root);
-    }
-
-    public static @NotNull Command fromMethod(@NotNull Method method, @NotNull Object object) {
-        com.wizardlybump17.wlib.command.annotation.Command annotation = method.getDeclaredAnnotation(com.wizardlybump17.wlib.command.annotation.Command.class);
-        if (annotation == null)
-            throw new IllegalArgumentException("Method " + method.getName() + " does not have a Command annotation");
-
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        if (parameterTypes.length == 0)
-            throw new IllegalArgumentException("Method " + method.getName() + " does not have any parameter");
-
-        if (!parameterTypes[0].equals(CommandContext.class))
-            throw new IllegalArgumentException("Method " + method.getName() + "'s first parameter is not a CommandContext");
-
-        String[] parts = annotation.value().split(" ");
-
-        int parameterIndex = 1;
-        CommandNode<?> lastNode = null;
-        for (int i = parts.length - 1; i >= 0; i--) {
-            String part = parts[i];
-
-            CommandNodeExecutor<?> executor;
-            if (i + 1 >= parts.length) {
-                executor = new CommandNodeExecutor<>() {
-                    @NotNull MethodHandle methodHandle; {
-                        try {
-                            methodHandle = MethodHandles.publicLookup().findVirtual(object.getClass(), method.getName(), MethodType.methodType(method.getReturnType(), method.getParameterTypes()));
-                        } catch (NoSuchMethodException | IllegalAccessException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-
-                    @SuppressWarnings("unchecked")
-                    @Override
-                    public @NotNull CommandResult<Object> execute(@NotNull CommandContext context) {
-                        List<Object> arguments = new ArrayList<>();
-
-                        arguments.add(object);
-                        arguments.add(context);
-
-                        arguments.addAll(context.arguments().getArguments().values()
-                                .stream()
-                                .filter(argument -> !(argument.node() instanceof LiteralCommandNode))
-                                .map(CommandContext.CommandNodeArgument::data)
-                                .toList()
-                        );
-
-                        try {
-                            Object invoke = methodHandle.invokeWithArguments(arguments);
-                            return invoke instanceof CommandResult<?> result ? (CommandResult<Object>) result : CommandResult.successful(context, invoke);
-                        } catch (Throwable e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                };
-            } else {
-                executor = null;
-            }
-
-            CommandNode<?> newNode;
-
-            if (part.charAt(0) == '<' && part.charAt(part.length() - 1) == '>') {
-                String nodeName = part.substring(1, part.length() - 1);
-
-                Class<?> parameterType = parameterTypes[parameterIndex++];
-                if (parameterType == int.class || parameterType == Integer.class)
-                    newNode = new IntegerCommandNode(nodeName, lastNode == null ? List.of() : List.of(lastNode), new AllowedNumberInputs.AllowedIntegerInputs.Unlimited(), executor);
-                else
-                    throw new IllegalArgumentException("Just trying stuff for now. Come back later");
-
-            } else {
-                newNode = new LiteralCommandNode(part, lastNode == null ? List.of() : List.of(lastNode), executor);
-            }
-
-            lastNode = newNode;
-        }
-
-        return new Command((LiteralCommandNode) lastNode);
     }
 }
