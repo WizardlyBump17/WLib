062db40046d48062102d563b1a746fda101ae961
WizardlyBump17 <davicraft130@gmail.com>
2025-11-10 13:08:23 -0300

    added some more tests

--- commands/src/test/java/com/wizardlybump17/wlib/test/command/extractor/method/MethodCommandExtractorTests.java
+++ commands/src/test/java/com/wizardlybump17/wlib/test/command/extractor/method/MethodCommandExtractorTests.java
@@ -1,9 +1,11 @@
 package com.wizardlybump17.wlib.test.command.extractor.method;
 
 import com.wizardlybump17.wlib.command.Command;
+import com.wizardlybump17.wlib.command.context.CommandContext;
 import com.wizardlybump17.wlib.command.extractor.CommandExtractor;
 import com.wizardlybump17.wlib.command.extractor.method.MethodCommandExtractor;
 import com.wizardlybump17.wlib.command.node.LiteralCommandNode;
+import com.wizardlybump17.wlib.command.result.CommandResult;
 import com.wizardlybump17.wlib.command.sender.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
@@ -73,6 +75,133 @@ class MethodCommandExtractorTests {
 
         @com.wizardlybump17.wlib.command.annotation.Command("hello there hi")
         public void helloThereHi(@NotNull CommandSender<?> sender) {
+        }
+    }
+
+    @Test
+    void test1() {
+        Test1 object = new Test1();
+
+        List<Command> expected = new ArrayList<>(List.of(
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                MethodCommandExtractor.createExecutor(object, "hello", CommandContext.class)
+                        )
+                ),
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                List.of(
+                                        new LiteralCommandNode(
+                                                "world",
+                                                MethodCommandExtractor.createExecutor(object, "helloWorld", CommandContext.class)
+                                        )
+                                )
+                        )
+                ),
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                List.of(
+                                        new LiteralCommandNode(
+                                                "there",
+                                                List.of(
+                                                        new LiteralCommandNode(
+                                                                "hi",
+                                                                MethodCommandExtractor.createExecutor(object, "helloThereHi", CommandContext.class)
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        ));
+        List<Command> actual = CommandExtractor.METHOD.extract(object);
+
+        expected.sort(null);
+        actual.sort(null);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static class Test1 {
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello")
+        public void hello(@NotNull CommandContext context) {
+        }
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello world")
+        public void helloWorld(@NotNull CommandContext context) {
+        }
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello there hi")
+        public void helloThereHi(@NotNull CommandContext context) {
+        }
+    }
+
+    @Test
+    void test2() {
+        Test2 object = new Test2();
+
+        List<Command> expected = new ArrayList<>(List.of(
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                MethodCommandExtractor.createExecutor(object, "hello", CommandContext.class)
+                        )
+                ),
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                List.of(
+                                        new LiteralCommandNode(
+                                                "world",
+                                                MethodCommandExtractor.createExecutor(object, "helloWorld", CommandContext.class)
+                                        )
+                                )
+                        )
+                ),
+                new Command(
+                        new LiteralCommandNode(
+                                "hello",
+                                List.of(
+                                        new LiteralCommandNode(
+                                                "there",
+                                                List.of(
+                                                        new LiteralCommandNode(
+                                                                "hi",
+                                                                MethodCommandExtractor.createExecutor(object, "helloThereHi", CommandContext.class)
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        ));
+        List<Command> actual = CommandExtractor.METHOD.extract(object);
+
+        expected.sort(null);
+        actual.sort(null);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static class Test2 {
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello")
+        public @NotNull CommandResult<?> hello(@NotNull CommandContext context) {
+            return CommandResult.successful(context, null);
+        }
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello world")
+        public @NotNull CommandResult<?> helloWorld(@NotNull CommandContext context) {
+            return CommandResult.successful(context, null);
+        }
+
+        @com.wizardlybump17.wlib.command.annotation.Command("hello there hi")
+        public @NotNull CommandResult<?> helloThereHi(@NotNull CommandContext context) {
+            return CommandResult.successful(context, null);
         }
     }
 }
