0fb21d7dd4ddfdcf0ed0bc7a19aae6b4606255e9
WizardlyBump17 <davicraft130@gmail.com>
2025-11-24 23:45:12 -0300

    UUIDs

--- commands/src/main/java/com/wizardlybump17/wlib/command/input/AllowedListInputs.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/input/AllowedListInputs.java
@@ -1,10 +1,16 @@
 package com.wizardlybump17.wlib.command.input;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public interface AllowedListInputs<T> extends AllowedInputs<T> {
 
     @NotNull List<T> allowedValues();
+
+    @Override
+    default boolean isAllowed(@Nullable T input) {
+        return allowedValues().contains(input);
+    }
 }
