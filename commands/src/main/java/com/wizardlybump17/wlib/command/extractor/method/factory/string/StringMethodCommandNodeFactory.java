2ad62087a9cfc315da7526b17ef38028cfbbad3e
WizardlyBump17 <davicraft130@gmail.com>
2025-11-25 21:11:48 -0300

    CTRL C CTRL V moment

--- commands/src/main/java/com/wizardlybump17/wlib/command/extractor/method/factory/string/StringMethodCommandNodeFactory.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/extractor/method/factory/string/StringMethodCommandNodeFactory.java
@@ -18,7 +18,7 @@ public class StringMethodCommandNodeFactory extends MethodCommandNodeFactory {
     public @NotNull CommandNode<?> create(@NotNull Object object, @NotNull Method method, @NotNull Command commandAnnotation, @NotNull Parameter parameter, @NotNull String name, @Nullable CommandNode<?> root) {
         Class<?> type = parameter.getType();
         if (!isSupported(type))
-            throw new IllegalArgumentException("Unsupported type. We accept only primitive Strings: " + type);
+            throw new IllegalArgumentException("Unsupported type. We accept only Strings: " + type);
 
         return new StringCommandNode(
                 name,
