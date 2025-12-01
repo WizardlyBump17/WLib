31cf0fc8583191f4f51e1fa8fd0a24c1e09136ad
WizardlyBump17 <davicraft130@gmail.com>
2025-12-01 20:42:39 -0300

    properly loading the skull skin

--- core/src/main/java/com/wizardlybump17/wlib/item/handler/SkullMetaHandler.java
+++ core/src/main/java/com/wizardlybump17/wlib/item/handler/SkullMetaHandler.java
@@ -1,23 +1,20 @@
 package com.wizardlybump17.wlib.item.handler;
 
+import com.destroystokyo.paper.profile.PlayerProfile;
 import com.wizardlybump17.wlib.item.ItemBuilder;
 import com.wizardlybump17.wlib.item.handler.model.SkullMetaHandlerModel;
-import lombok.NonNull;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class SkullMetaHandler extends ItemMetaHandler<SkullMetaHandlerModel> {
-
-    private final @NonNull Map<String, PlayerProfile> profileCache = new ConcurrentHashMap<>();
 
     public SkullMetaHandler(SkullMetaHandlerModel model, ItemBuilder builder) {
         super(model, builder);
@@ -34,19 +31,19 @@ public class SkullMetaHandler extends ItemMetaHandler<SkullMetaHandlerModel> {
     @Override
     public void deserialize(Map<String, Object> map) {
         String skull = (String) map.get("skull");
-        if (skull == null)
-            return;
-
-        try {
-            skull(Bukkit.getOfflinePlayer(UUID.fromString(skull)));
-        } catch (IllegalArgumentException ignored) {
+        if (skull != null) {
             skull(skull);
+            return;
         }
+
+        String owner = (String) map.get("owner");
+        if (owner != null)
+            skull(Bukkit.getOfflinePlayer(UUID.fromString(owner)));
     }
 
     public String skullUrl() {
         return getBuilder().<String, SkullMeta>getFromMeta(meta -> {
-            PlayerProfile profile = meta.getOwnerProfile();
+            PlayerProfile profile = meta.getPlayerProfile();
             if (profile == null)
                 return null;
 
@@ -56,15 +53,20 @@ public class SkullMetaHandler extends ItemMetaHandler<SkullMetaHandlerModel> {
     }
 
     public SkullMetaHandler skull(String url) {
-        getBuilder().<SkullMeta>consumeMeta(meta -> meta.setOwnerProfile(profileCache.computeIfAbsent(url, $ -> {
+        getBuilder().<SkullMeta>consumeMeta(meta -> {
             try {
-                PlayerProfile profile = Bukkit.createPlayerProfile(UUID.nameUUIDFromBytes(url.getBytes()));
-                profile.getTextures().setSkin(URI.create(url).toURL());
-                return profile;
+                PlayerProfile profile = Bukkit.createProfile(UUID.nameUUIDFromBytes(url.getBytes()));
+
+                PlayerTextures textures = profile.getTextures();
+                textures.setSkin(URI.create(url).toURL());
+
+                profile.setTextures(textures);
+
+                meta.setPlayerProfile(profile);
             } catch (MalformedURLException e) {
                 throw new IllegalArgumentException("Invalid URL " + url, e);
             }
-        })));
+        });
         return this;
     }
 
