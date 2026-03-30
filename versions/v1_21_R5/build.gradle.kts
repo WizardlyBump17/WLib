406fca326b67ef020b299d3e585863ee7ea72e7f
WizardlyBump17 <davicraft130@gmail.com>
2026-03-29 21:33:03 -0300

    fixed the paperweight version

--- versions/v1_21_R5/build.gradle.kts
+++ versions/v1_21_R5/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.papermc.paperweight.userdev") version "2.0.0-SNAPSHOT"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
 }
 
 apply(plugin = "io.papermc.paperweight.userdev")
