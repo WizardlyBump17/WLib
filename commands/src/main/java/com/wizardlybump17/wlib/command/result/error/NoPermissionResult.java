1db834c8dae7b52eb380976beb5829faba896313
WizardlyBump17 <davicraft130@gmail.com>
2026-05-19 22:01:20 -0300

    deprecated the NoPermissionResult

--- commands/src/main/java/com/wizardlybump17/wlib/command/result/error/NoPermissionResult.java
+++ commands/src/main/java/com/wizardlybump17/wlib/command/result/error/NoPermissionResult.java
@@ -3,6 +3,10 @@ package com.wizardlybump17.wlib.command.result.error;
 import com.wizardlybump17.wlib.command.node.CommandNode;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @deprecated use {@link UnauthorizedResult} or {@link ForbiddenResult}
+ */
+@Deprecated
 public record NoPermissionResult<T>(int lastInputIndex, @NotNull CommandNode<?> lastNode) implements UnsuccessResult<T> {
 
     public static final @NotNull String ID = "WLib:Unsuccess/NoPermission";
